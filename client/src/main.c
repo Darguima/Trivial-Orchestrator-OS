16a776a915f9dfb5a9fa4e89c7a1ff2333fb9815
afooonso <afonsopedreira14@gmail.com>
2024-04-25 18:44:57 +0100

    Connection Between Server And Client Established

--- client/src/main.c
+++ client/src/main.c
@@ -1,6 +1,48 @@
 #include "hello_world/hello_world.h"
 
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#define SERVER_FIFO "../tmp/fifo_server"
+
 int main() {
-  hello_world();
-  return 0;
+  int server_fd, client_fd;
+  char client_fifo[256], buffer[300];
+  sprintf(client_fifo, "../tmp/fifo_client_%d", getpid());
+
+  if (mkfifo(client_fifo, 0666) == -1) {
+    perror("Error creating client FIFO");
+    exit(EXIT_FAILURE);
+  }
+
+  server_fd = open(SERVER_FIFO, O_WRONLY);
+  if (server_fd == -1) {
+    perror("Erro ao abrir FIFO do servidor");
+    unlink(client_fifo);
+    exit(EXIT_FAILURE);
+  }
+
+  sprintf(buffer, "%s %s", client_fifo, "execute");
+  write(server_fd, buffer, strlen(buffer) + 1);
+  close(server_fd);
+
+  client_fd = open(client_fifo, O_RDONLY);
+  if (client_fd == -1) {
+    perror("Error opening client FIFO");
+    unlink(client_fifo);
+    exit(EXIT_FAILURE);
+  }
+
+  if (read(client_fd, buffer, sizeof(buffer)) > 0) {
+    printf("ID of the task: %s\n", buffer);
+  }
+
+  close(client_fd);
+  unlink(client_fifo);
+
+  return EXIT_SUCCESS;
 }
--- server/src/main.c
+++ server/src/main.c
@@ -1,6 +1,91 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include "hello_world/hello_world.h"
 
+#define SERVER_FIFO "../tmp/fifo_server"
+#define SERVER_LOG "../tmp/server_log.txt"
+#define MAX_TASKS 100
+
+typedef enum { PENDING, EXECUTING, COMPLETED } EstadoTarefa;
+
+typedef struct {
+  int id;
+  EstadoTarefa estado;
+} Tarefa;
+
+Tarefa tarefas[MAX_TASKS];
+int task_count = 0;
+
+void execute_task(int task_id) {
+  int fd;
+  pid_t pid;
+
+  tarefas[task_id].estado = EXECUTING;
+
+  pid = fork();
+  if (pid == -1) {
+    perror("Error creating child process");
+    exit(EXIT_FAILURE);
+  } else if (pid == 0) {
+    fd = open(SERVER_LOG, O_WRONLY | O_CREAT | O_APPEND, 0666);
+    if (fd == -1) {
+      perror("Error opening log file");
+      exit(EXIT_FAILURE);
+    }
+    dup2(fd, STDOUT_FILENO);
+    dup2(fd, STDERR_FILENO);
+    close(fd);
+
+    execlp("ls", "ls", "-l", NULL);
+    perror("Error executing command");
+    exit(EXIT_FAILURE);
+  } else {
+    wait(NULL);
+    tarefas[task_id].estado = COMPLETED;
+  }
+}
+
 int main() {
-  hello_world();
-  return 0;
+  int server_fd, client_fd;
+  char buffer[1024], client_fifo[256], response[256];
+
+    if (mkfifo(SERVER_FIFO, 0666) == -1) {
+        perror("Error creating server FIFO");
+        exit(EXIT_FAILURE);
+    }
+
+
+  server_fd = open(SERVER_FIFO, O_RDONLY);
+  if (server_fd == -1) {
+    perror("Error opening server FIFO");
+    exit(EXIT_FAILURE);
+  }
+
+  printf("Server Starting...\n Hold on for a moment \n Server Started\n Waiting for clients...\n");
+  while (1) {
+    if (read(server_fd, buffer, sizeof(buffer)) > 0) {
+      sscanf(buffer, "%s", client_fifo);
+
+      tarefas[task_count].id = task_count;
+      tarefas[task_count].estado = PENDING;
+      sprintf(response, "%d", task_count);
+      client_fd = open(client_fifo, O_WRONLY);
+      write(client_fd, response, strlen(response) + 1);
+      close(client_fd);
+
+      execute_task(task_count);
+      task_count++;
+    }
+  }
+
+  close(server_fd);
+  unlink(SERVER_FIFO);
+
+  return EXIT_SUCCESS;
 }
