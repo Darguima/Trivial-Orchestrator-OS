642d4ec916e728cb57a2fe88a1b299da75b6317f
Mack <42896437+Mackgame4@users.noreply.github.com>
2024-05-01 00:16:27 +0100

    Dg/orchestrator (#7)
    * FCFS almost working (need some refactor)
    
    * add ifndef on headers files
    
    * Code refactor and README
    
    * using scheduler on the main
    
    * freeing all memory
    
    ---------
    
    Co-authored-by: Dário Guimarães <dsgdevbraga@gmail.com>

--- server/src/main.c
+++ server/src/main.c
@@ -1,3 +1,32 @@
+#include "scheduler/scheduler.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+int main() {
+  Scheduler scheduler = create_scheduler(FCFS);
+
+  char command[100];
+  int process_id;
+  Process process;
+
+  for (int i = 0; i < 14; i++) {
+    sprintf(command, "ls %d", i);
+    process_id = enqueue_process(scheduler, command, 10);
+    printf("Enqueued Process %d: %s\n", process_id, command);
+  }
+
+  for (int i = 0; i < 4; i++) {
+    process = dequeue_process(scheduler);
+    printf("Dequeued Process %d: %s\n", process->id, process->command);
+    free(process->command);
+    free(process);
+  }
+
+  destroy_scheduler(scheduler);
+
+  return 0;
+ }
+/*
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -49,4 +78,4 @@ int main() {
     close(s_fd);
     free(buffer);
     return 0;
-}
+}*/
